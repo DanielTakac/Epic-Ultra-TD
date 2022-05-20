ea65dbab83479b63d20abdace7048992e74d66ab
DanielTakac <danieltakac@protonmail.com>
2022-05-20 19:31:00 +0200

    Updated MinigunBoost.cs

--- Assets/Scripts/MinigunBoost.cs
+++ Assets/Scripts/MinigunBoost.cs
@@ -4,19 +4,95 @@ using UnityEngine;
 
 public class MinigunBoost : MonoBehaviour{
 
-    void Start(){
+    [Header("Setup")]
 
+    public Texture2D whiteCursor;
 
-        
+    private SoundManager soundManager;
+
+    private GameObject[] enemies;
+
+    public MeshRenderer mesh1;
+
+    private Material originalMat1;
+
+    public Material hoverMat;
+
+    public bool activated = false;
+
+    private bool stage1 = false;
+
+    private void OnMouseEnter() {
+
+        if (BoostShop.boostSelected) {
+
+            mesh1.material = hoverMat;
+
+        }
+
     }
 
-    void Update(){
+    private void OnMouseExit() {
 
-        /*if (Input.GetKeyDown("space")){
+        if (BoostShop.boostSelected) {
+
+            mesh1.material = originalMat1;
+
+        }
+
+    }
+
+    private void OnMouseDown() {
+
+        Cursor.SetCursor(whiteCursor, Vector2.zero, CursorMode.ForceSoftware);
+
+        if (BoostShop.boostSelected && activated == false) {
+
+            mesh1.material = originalMat1;
+
+            BoostShop.boostSelected = false;
+
+            BoostShop.boosts--;
 
             Activate();
 
-        }*/
+        }
+
+    }
+
+    void Start() {
+
+        originalMat1 = mesh1.material;
+
+        soundManager = FindObjectOfType<SoundManager>();
+
+    }
+
+    void Update() {
+
+        if (Input.GetKeyDown("space")){
+
+            Activate();
+
+        }
+
+    }
+
+    public void Activate() {
+
+        if (activated) return;
+
+        gameObject.GetComponent<MinigunTurret>().canShoot = false;
+
+        activated = true;
+
+        Stage1();
+
+    }
+
+    private void Stage1() {
+
+        stage1 = true;
 
     }
 }
