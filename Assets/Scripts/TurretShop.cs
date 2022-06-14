6457bd31a4821b5d0fcef5046eb5e87eeb1dc245
DanielTakac <danieltakac@protonmail.com>
2022-06-14 09:15:08 +0200

    .

--- Assets/Scripts/TurretShop.cs
+++ Assets/Scripts/TurretShop.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TurretShop : MonoBehaviour{
@@ -7,5 +8,21 @@ public class TurretShop : MonoBehaviour{
     public static bool moneyClicked = false;
     public static bool minigunClicked = false;
     public static bool missileClicked = false;
+
+    public static Dictionary<string, bool> turretsClicked = new Dictionary<string, bool> {
+        { "voyager", false },
+        { "tesla", false },
+        { "money", false },
+        { "minigun", false },
+        { "missile", false }
+    };
+
+    // public static Dictionary<> turretInfos
+
+    public static void CancelBuilding(string turretName) {
+
+        turretsClicked[turretName] = false;
+
+    }
 
 }
