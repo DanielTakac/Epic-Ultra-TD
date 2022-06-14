1bbd6a8e5454a219ca0e8a00c2d6ef27f8bdadf5
DanielTakac <danieltakac@protonmail.com>
2022-06-14 11:17:34 +0200

    Big progress PogU

--- Assets/Scripts/TurretShop.cs
+++ Assets/Scripts/TurretShop.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class TurretShop : MonoBehaviour {
@@ -13,8 +14,10 @@ public class TurretShop : MonoBehaviour {
 
     // I had to create a serialized array and put the values from it to the dictionary because unity is retarded and can't serialize a dictionary
     [SerializeField] private GameObject[] turretInfosArray;
+    [SerializeField] private GameObject[] turretButtonsArray;
 
     private Dictionary<string, GameObject> turretInfos = new Dictionary<string, GameObject>();
+    private Dictionary<string, GameObject> turretButtons = new Dictionary<string, GameObject>();
 
     private Dictionary<string, bool> turretsClicked = new Dictionary<string, bool> {
         { "voyager", false },
@@ -23,6 +26,9 @@ public class TurretShop : MonoBehaviour {
         { "minigun", false },
         { "missile", false }
     };
+
+    [SerializeField] private Color turretPressedColor;
+    [SerializeField] private Color turretNotPressedColor;
 
     private void Start() {
 
@@ -39,6 +45,19 @@ public class TurretShop : MonoBehaviour {
         turretInfos.Add("minigun", turretInfosArray[3]);
         turretInfos.Add("missile", turretInfosArray[4]);
 
+        if (turretButtonsArray.Length != 5) {
+
+            Debug.LogError("Not all turret buttons added to turretButtonsArray!");
+            return;
+
+        }
+
+        turretButtons.Add("voyager", turretButtonsArray[0]);
+        turretButtons.Add("tesla", turretButtonsArray[1]);
+        turretButtons.Add("money", turretButtonsArray[2]);
+        turretButtons.Add("minigun", turretButtonsArray[3]);
+        turretButtons.Add("missile", turretButtonsArray[4]);
+
     }
 
     public void CancelBuilding(string turretName) => turretsClicked[turretName] = false;
@@ -49,9 +68,56 @@ public class TurretShop : MonoBehaviour {
 
     public void ClickTurretIcon(string turretName) {
 
+        // Return if another turret is already selected
+        foreach (KeyValuePair<string, bool> turretClicked in turretsClicked) {
+
+            if (turretClicked.Value && turretClicked.Key != turretName) {
+
+                Debug.LogError("Another turret already selected");
+
+                return;
+
+            }
+
+        }
+
+        if (turretsClicked[turretName]) {
+
+            // Unselect
+
+        } else {
+
+            // Select
+
+        }
+
         turretsClicked[turretName] = true;
 
         cancelBuildingObject.SetActive(true);
+
+        var colors = turretButtons[turretName].GetComponent<Button>().colors;
+
+        if (turretsClicked[turretName]) {
+
+            colors.normalColor = turretPressedColor;
+            colors.highlightedColor = turretPressedColor;
+            colors.pressedColor = turretPressedColor;
+            colors.selectedColor = turretPressedColor;
+            colors.disabledColor = turretPressedColor;
+
+        } else {
+
+            colors.normalColor = turretNotPressedColor;
+            colors.highlightedColor = turretNotPressedColor;
+            colors.pressedColor = turretNotPressedColor;
+            colors.selectedColor = turretNotPressedColor;
+            colors.disabledColor = turretNotPressedColor;
+
+        }
+
+        turretButtons[turretName].GetComponent<Button>().colors = colors;
+
+        Debug.LogError(turretName);
 
     }
 
